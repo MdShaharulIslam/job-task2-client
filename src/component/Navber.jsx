26a3ff739fca5769dd9eec08ba29ef92c2463110
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-17 15:40:00 +0600

    change name

--- src/component/Navber.jsx
+++ src/component/Navber.jsx
@@ -14,7 +14,7 @@ import {
 import useAuth from "../Hooks/useAuth";
 import { Link } from "react-router-dom";
 
-import logo from "../assets/app-store.png";
+import logo from "../assets/sp logo.jpg";
 
 const profileMenuItems = [
  
@@ -89,7 +89,7 @@ export default function TopNavbar() {
     <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900">
         <Typography as="a" href="#" className="mr-4 cursor-pointer flex justify-center items-center gap-2 text-xl py-1.5 font-bold">
-         <img className="w-4" src={logo} alt="" /> We <span className="text-primary">Prod</span>
+         <img className="w-4" src={logo} alt="" />S <span className="text-primary">Prod</span>
         </Typography>
         <div className="flex items-center gap-4">
           
