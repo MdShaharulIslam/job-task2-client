517f4c98025008ffae55b40ee55316206249ad30
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-18 08:30:04 +0600

    added axios link

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const axiosCommon = axios.create({
- 
-  //  baseURL: "http://localhost:5000",
  baseURL:"https://job-task2-server.vercel.app/",
 });
-
 const useAxios = () => {
   return axiosCommon;
 };
-
 export default useAxios;
