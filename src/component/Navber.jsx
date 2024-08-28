ce8f2bbab1759cadf834fa3fcc507d97f55a516d
Md shaharul Islam <shaharulislam025@gmail.com>
2024-08-28 08:51:04 +0600

    some adde

--- src/component/Navber.jsx
+++ src/component/Navber.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   Navbar,
-  MobileNav,
+  Collapse,
   Typography,
   Button,
   IconButton,
@@ -17,7 +17,6 @@ import { Link } from "react-router-dom";
 import logo from "../assets/sp logo.jpg";
 
 const profileMenuItems = [
- 
   { label: "Sign Out" },
 ];
 
@@ -25,7 +24,8 @@ function ProfileMenu() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const closeMenu = () => setIsMenuOpen(false);
-const {user,logOut}=useAuth()
+  const { user, logOut } = useAuth();
+
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
       <MenuHandler>
@@ -57,7 +57,7 @@ const {user,logOut}=useAuth()
               }`}
             >
               <Typography
-              onClick={logOut}
+                onClick={logOut}
                 as="span"
                 variant="small"
                 className="font-normal"
@@ -83,67 +83,100 @@ export default function TopNavbar() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  
-
   return (
     <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography as="a" href="#" className="mr-4 cursor-pointer flex justify-center items-center gap-2 text-xl py-1.5 font-bold">
-         <img className="w-4" src={logo} alt="" />S <span className="text-primary">Prod</span>
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 cursor-pointer flex justify-center items-center gap-2 text-xl py-1.5 font-bold"
+        >
+          <img className="w-4" src={logo} alt="" />
+          S <span className="text-primary">Prod</span>
         </Typography>
         <div className="flex items-center gap-4">
-          
           <div className="flex items-center gap-x-1">
             {user ? (
               <ProfileMenu />
             ) : (
               <>
-            <Link to='/login'>
-                  <Button variant="text" size="sm" className="hidden lg:inline-block">
+                <Link to="/login">
+                  <Button
+                    variant="text"
+                    size="sm"
+                    className="hidden lg:inline-block"
+                  >
                     <span>Log In</span>
                   </Button>
-            </Link>
-           <Link to='/register'>
-                  <Button variant="gradient" color="green" size="sm" className="hidden lg:inline-block">
+                </Link>
+                <Link to="/register">
+                  <Button
+                    variant="gradient"
+                    color="green"
+                    size="sm"
+                    className="hidden lg:inline-block"
+                  >
                     <span>Sign In</span>
                   </Button>
-           </Link>
+                </Link>
               </>
             )}
           </div>
-{!user &&          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            )}
-          </IconButton>}
+          {!user && (
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          )}
         </div>
       </div>
-      <MobileNav open={openNav}>
-       
+      <Collapse open={openNav}>
         <div className="flex items-center gap-x-1">
-        <Link to='/login'>
+          <Link to="/login">
             <Button fullWidth variant="text" size="sm">
               <span>Log In</span>
             </Button>
-        </Link>
-         <Link to='/register'>
+          </Link>
+          <Link to="/register">
             <Button fullWidth variant="gradient" size="sm">
               <span>Sign In</span>
             </Button>
-         </Link>
+          </Link>
         </div>
-      </MobileNav>
+      </Collapse>
     </Navbar>
   );
 }
